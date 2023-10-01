1c0cc7d9ff6fa8799afdb72d1ed0196ee5199a7e
Juanzq10dev <juanzq10dev@gmail.com>
2023-10-01 17:37:24 -0400

    First version of main file added

---
+++ dotnet_client/src/pages/main.tsx
@@ -0,0 +1,10 @@
+import Card from "@/components/card";
+
+export default function Main() {
+    return (
+        <div>
+            <h1>TO DO APP</h1>
+            < Card />
+        </div>
+    );
+}
